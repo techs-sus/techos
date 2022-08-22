5fba19b0819892a85c56ca2e67318615472d16a6
techs-sus <92276908+techs-sus@users.noreply.github.com>
2022-08-22 12:56:28 -0400

    dev: scroll v3

--- src/init.lua
+++ src/init.lua
@@ -1,4 +1,4 @@
 local UI = require(script.createGUI)
-local API = require(script.api)(UI.main)
+local API = require(script.api)(UI.scroll)
 -- attempt to create a window (For Testing.)
 UI.part.CFrame = CFrame.new(0, 10, -10)
