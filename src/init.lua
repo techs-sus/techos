6ce006bc7646867c4abee352bbbcaf73e34c4d11
techs-sus <92276908+techs-sus@users.noreply.github.com>
2022-08-23 10:32:06 -0400

    dev: add banner

--- src/init.lua
+++ src/init.lua
@@ -1,5 +1,11 @@
 local UI = require(script.createGUI)
 local API = require(script.api)(UI.scroll, UI.list)
--- attempt to create a window (For Testing.)
+local commandManager = require(script.commandManager)
+commandManager.addDefaultCommands()
 UI.part.CFrame = CFrame.new(0, 10, -10)
-API.createText("hi")
+API.createText("<font color=rgb(50,50,50)>#### banner ####<font>")
+API.createText(
+	string.format("hello %s, welcome to techos, commit %s", getfenv(1).owner.Name, getfenv(1).__COMPILEINFO.commit)
+)
+API.createText("techos is still in development, some features are missing")
+API.createText("repository: techs-sus/techos")
