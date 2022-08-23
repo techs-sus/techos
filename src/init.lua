983493620fdeb0632240268a1d70e84e3bb7eed6
techs-sus <92276908+techs-sus@users.noreply.github.com>
2022-08-23 10:43:46 -0400

    dev: cleanup

--- src/init.lua
+++ src/init.lua
@@ -1,12 +1,17 @@
 local UI = require(script.createGUI)
 local API = require(script.api)(UI.scroll, UI.list)
 local commandManager = require(script.commandManager)
+local owner: Player = getfenv(1).owner
 commandManager.addDefaultCommands()
 UI.part.CFrame = CFrame.new(0, 10, -10)
-API.createText("#### banner ####")
+API.createText("#### banner ####").TextColor3 = Color3.new(1, 0, 0)
 API.createText(
-	string.format("hello %s, welcome to techos, commit %s", getfenv(1).owner.Name, getfenv(1).__COMPILEINFO.commit)
-).TextColor3 =
-	Color3.new(1, 0, 0)
+	string.format(
+		"hello %s, welcome to techos, commit %s",
+		owner.Name,
+		string.sub(getfenv(1).__COMPILEINFO.commit, 1, 7)
+	)
+)
 API.createText("techos is still in development, some features are missing")
 API.createText("repository: techs-sus/techos")
+API.createText("for development: g/ps/techos-dev")
